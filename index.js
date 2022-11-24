213ae34a750bddfeb4db49b193fcd7d45bf15395
Mirza Omercic <38662264+Mirza258@users.noreply.github.com>
2022-11-24 13:30:46 +0100

    Added color changer

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
-let gridSize = 10;
+let gridSize = 16;
 let clicked = false;
 const grid = document.getElementById("grid");
+const colorPicker = document.getElementById("colorpicker");
 grid.addEventListener('mousedown', function changeClicked(){
     clicked = true;
 })
+
+let color = colorPicker.value;
+
+// colorPicker.addEventListener("input", updateFirst);
+colorPicker.addEventListener('change', function watchColorPicker(){ color = colorPicker.value})
 
 for(let i = 0; i < gridSize*gridSize; i++){
     const el = document.createElement("div");
     let x = 100/gridSize;
     el.style.width = `${x}%`;
     el.style.height = `${x}%`;
-    el.style.background = "red";
+    el.style.background = "white";
     const get = document.getElementById("grid");
     get.appendChild(el);
     el.addEventListener('click', function changeColor(){
-        el.style.background = "white";    
+        el.style.background = color;    
     });
     el.addEventListener('mouseover', function changeColor(){
         if(clicked === true){
-            el.style.background = "white";
+            el.style.background = color;
         }
     });
 }
